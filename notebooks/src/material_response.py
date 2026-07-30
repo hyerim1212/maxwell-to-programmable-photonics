822973a6f40f4823041213e34591bbf94adf24d1
hyerim1212 <hyerim12@dgist.ac.kr>
2026-07-30 22:41:41 +0900

    update notes and add a tool

---
+++ notebooks/src/material_response.py
@@ -0,0 +1,32 @@
+def uniform_wire_response(voltage, length, radius, conductivity):
+
+
+
+
+
+    
+def dielectric_capacitor_fixed_charge(free_surface_charge_density, area, separation, relative_permittivity,):
+
+
+
+
+
+
+
+
+
+
+
+
+def layered_dielectric_capacitor(voltage, area, thicknesses, relative_permittivities):
+
+
+
+
+
+
+
+
+
+    
+def relative_residual(reference, comparison):
